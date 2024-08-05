db666b6a88777767aa4297bc29c390c261affd6d
Simon Jarrett <simon.jarrett@nesta.org.uk>
2024-08-05 16:59:12 +0100

    Refactor components

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,9 +5,16 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim,blade}'
+    './app/views/**/*.{erb,haml,html,slim,rblade}'
   ],
   theme: {
+    boxShadow: {
+      "0": "0 0 rgb(0,0,0)",
+      "1": "0 1px rgb(0,0,0)",
+      "2": "0 2px rgb(0,0,0)",
+      "3": "0 3px rgb(0,0,0)",
+      "4": "0 4px rgb(0,0,0)",
+    },
     extend: {
       colors: {
         "focus": "#9aff3c"
